54f444fa3c7759a330b8c5192dcd07e1fa0421ca
patrickhock <patrick.hock-extern@deutschebahn.com>
2021-02-12 16:26:52 +0100

    asdf

---
+++ jobs/init.groovy
@@ -0,0 +1,29 @@
+pipelineJob('Build-Pipeline') {
+    parameters {
+        booleanParam('skipTests', 'develop')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    branch('*/master')
+                    remote {
+                        url('https://github.com/hawkpatrick/sample-java-project.git')
+                    }
+                    extensions {}
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+    concurrentBuild(false)
+    configure {
+        it / definition / lightweight(true)
+    }
+    logRotator {
+        daysToKeep(0)
+        daysToKeep(1)
+    }
+
+}
+
